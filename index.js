904a5413a0666395fa01c882c84edbb2e21034d1
pizzacode-byte <78670280+pizzacode-byte@users.noreply.github.com>
2021-02-11 18:43:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function love(){
+    var text = $('#text').val();
+    phrases = ['Ti amo per tutto ciò che sei, tutto ciò che sei stato, tutto ciò che devi ancora essere.', 'Tu guardi le stelle, stella mia, ed io vorrei essere il cielo per guardare te con mille occhi.','Dubita che le stelle siano fuoco, dubita che il sole si muova, dubita che la verità sia mentitrice, ma non dubitare mai del mio amore.'];
+
+    $('#text').html(phrases[Math.floor(Math.random() * phrases.length)]);
+}
